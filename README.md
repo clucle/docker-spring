18cdc0f38f587032eefb0320e4ad62e8d28a5ae7
clucle <wjdenwls123@gmail.com>
2018-11-25 21:18:07 +0900

    no
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 ## Usage
 
 ```bash
+$ mvn clean install
 $ docker build -f Dockerfile -t docker-spring .
 $ docker run -p 1124:1124
+```
+
+## Database
+
+```bash
+docker pull mysql
+docker run --name docker-spring-mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=db-name -e MYSQL_USER=user-name -e MYSQL_PASSWORD=user-password -d mysql:latest
+docker rm docker-spring-mysql
 ```
---
+++ src/main/java/com/github/clucle/dockerspring/Controller/UserController.java
@@ -0,0 +1,30 @@
+package com.github.clucle.dockerspring.Controller;
+
+import com.github.clucle.dockerspring.Models.User;
+import com.github.clucle.dockerspring.Models.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewUser(@RequestParam String name) {
+        User u = new User();
+        u.setName(name);
+        userRepository.save(u);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
